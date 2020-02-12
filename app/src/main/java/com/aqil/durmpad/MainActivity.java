0795121ed53490918bf5f6f54ce2ba06fe08d97d
Admin <aqilzeka99@gmail.com>
2020-02-13 02:35:13 +0400

    Initial commit

---
+++ app/src/main/java/com/aqil/durmpad/MainActivity.java
@@ -0,0 +1,53 @@
+package com.aqil.durmpad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void playsound1 (View v) {
+
+    }
+
+    public void playsound2 (View v) {
+
+    }
+
+    public void playsound3 (View v) {
+
+    }
+
+    public void playsound4 (View v) {
+
+    }
+
+    public void playsound5 (View v) {
+
+    }
+
+    public void playsound6 (View v) {
+
+    }
+
+    public void playsound7 (View v) {
+
+    }
+
+    public void playsound8 (View v) {
+
+    }
+
+    public void playsound9 (View v) {
+
+    }
+
+
+}
